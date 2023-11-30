c8c698a8af07408eaa882a84fb203fcea76a5bab
joelsnz <joel04slm@gmail.com>
2023-11-30 19:00:47 +0100

    work

---
+++ CIN/Proyectos/ExamenCIN/ExamenCIN/Form1.Designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.GroupBoxControlPuerto = New System.Windows.Forms.GroupBox()
+        Me.ButtonDesconectar = New System.Windows.Forms.Button()
+        Me.ButtonConectar = New System.Windows.Forms.Button()
+        Me.SerialPort = New System.IO.Ports.SerialPort(Me.components)
+        Me.GroupBoxSalida0 = New System.Windows.Forms.GroupBox()
+        Me.ButtonSalida0Off = New System.Windows.Forms.Button()
+        Me.ButtonSalida0On = New System.Windows.Forms.Button()
+        Me.GroupBoxSalida1 = New System.Windows.Forms.GroupBox()
+        Me.ButtonSalida1Off = New System.Windows.Forms.Button()
+        Me.ButtonSalida1On = New System.Windows.Forms.Button()
+        Me.GroupBoxControlPuerto.SuspendLayout()
+        Me.GroupBoxSalida0.SuspendLayout()
+        Me.GroupBoxSalida1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBoxControlPuerto
+        '
+        Me.GroupBoxControlPuerto.Controls.Add(Me.ButtonDesconectar)
+        Me.GroupBoxControlPuerto.Controls.Add(Me.ButtonConectar)
+        Me.GroupBoxControlPuerto.Font = New System.Drawing.Font("Roboto Mono", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBoxControlPuerto.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBoxControlPuerto.Name = "GroupBoxControlPuerto"
+        Me.GroupBoxControlPuerto.Size = New System.Drawing.Size(343, 74)
+        Me.GroupBoxControlPuerto.TabIndex = 1
+        Me.GroupBoxControlPuerto.TabStop = False
+        Me.GroupBoxControlPuerto.Text = "CONTROL DE PUERTO"
+        '
+        'ButtonDesconectar
+        '
+        Me.ButtonDesconectar.Location = New System.Drawing.Point(169, 29)
+        Me.ButtonDesconectar.Name = "ButtonDesconectar"
+        Me.ButtonDesconectar.Size = New System.Drawing.Size(163, 31)
+        Me.ButtonDesconectar.TabIndex = 2
+        Me.ButtonDesconectar.Text = "Disconnected"
+        Me.ButtonDesconectar.UseVisualStyleBackColor = True
+        '
+        'ButtonConectar
+        '
+        Me.ButtonConectar.Location = New System.Drawing.Point(11, 29)
+        Me.ButtonConectar.Name = "ButtonConectar"
+        Me.ButtonConectar.Size = New System.Drawing.Size(152, 31)
+        Me.ButtonConectar.TabIndex = 1
+        Me.ButtonConectar.Text = "Connected"
+        Me.ButtonConectar.UseVisualStyleBackColor = True
+        '
+        'GroupBoxSalida0
+        '
+        Me.GroupBoxSalida0.Controls.Add(Me.ButtonSalida0Off)
+        Me.GroupBoxSalida0.Controls.Add(Me.ButtonSalida0On)
+        Me.GroupBoxSalida0.Font = New System.Drawing.Font("Roboto Mono", 10.2!, System.Drawing.FontStyle.Bold)
+        Me.GroupBoxSalida0.Location = New System.Drawing.Point(12, 92)
+        Me.GroupBoxSalida0.Name = "GroupBoxSalida0"
+        Me.GroupBoxSalida0.Size = New System.Drawing.Size(343, 75)
+        Me.GroupBoxSalida0.TabIndex = 2
+        Me.GroupBoxSalida0.TabStop = False
+        Me.GroupBoxSalida0.Text = "Salida 0"
+        '
+        'ButtonSalida0Off
+        '
+        Me.ButtonSalida0Off.Location = New System.Drawing.Point(163, 30)
+        Me.ButtonSalida0Off.Name = "ButtonSalida0Off"
+        Me.ButtonSalida0Off.Size = New System.Drawing.Size(169, 36)
+        Me.ButtonSalida0Off.TabIndex = 2
+        Me.ButtonSalida0Off.Text = "Button2"
+        Me.ButtonSalida0Off.UseVisualStyleBackColor = True
+        '
+        'ButtonSalida0On
+        '
+        Me.ButtonSalida0On.Location = New System.Drawing.Point(11, 30)
+        Me.ButtonSalida0On.Name = "ButtonSalida0On"
+        Me.ButtonSalida0On.Size = New System.Drawing.Size(146, 36)
+        Me.ButtonSalida0On.TabIndex = 0
+        Me.ButtonSalida0On.Text = "Button1"
+        Me.ButtonSalida0On.UseVisualStyleBackColor = True
+        '
+        'GroupBoxSalida1
+        '
+        Me.GroupBoxSalida1.Controls.Add(Me.ButtonSalida1Off)
+        Me.GroupBoxSalida1.Controls.Add(Me.ButtonSalida1On)
+        Me.GroupBoxSalida1.Font = New System.Drawing.Font("Roboto Mono", 10.2!, System.Drawing.FontStyle.Bold)
+        Me.GroupBoxSalida1.Location = New System.Drawing.Point(12, 173)
+        Me.GroupBoxSalida1.Name = "GroupBoxSalida1"
+        Me.GroupBoxSalida1.Size = New System.Drawing.Size(343, 75)
+        Me.GroupBoxSalida1.TabIndex = 3
+        Me.GroupBoxSalida1.TabStop = False
+        Me.GroupBoxSalida1.Text = "Salida 1"
+        '
+        'ButtonSalida1Off
+        '
+        Me.ButtonSalida1Off.Location = New System.Drawing.Point(163, 30)
+        Me.ButtonSalida1Off.Name = "ButtonSalida1Off"
+        Me.ButtonSalida1Off.Size = New System.Drawing.Size(169, 36)
+        Me.ButtonSalida1Off.TabIndex = 2
+        Me.ButtonSalida1Off.Text = "Button2"
+        Me.ButtonSalida1Off.UseVisualStyleBackColor = True
+        '
+        'ButtonSalida1On
+        '
+        Me.ButtonSalida1On.Location = New System.Drawing.Point(11, 30)
+        Me.ButtonSalida1On.Name = "ButtonSalida1On"
+        Me.ButtonSalida1On.Size = New System.Drawing.Size(146, 36)
+        Me.ButtonSalida1On.TabIndex = 0
+        Me.ButtonSalida1On.Text = "Button1"
+        Me.ButtonSalida1On.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(363, 257)
+        Me.Controls.Add(Me.GroupBoxSalida1)
+        Me.Controls.Add(Me.GroupBoxSalida0)
+        Me.Controls.Add(Me.GroupBoxControlPuerto)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBoxControlPuerto.ResumeLayout(False)
+        Me.GroupBoxSalida0.ResumeLayout(False)
+        Me.GroupBoxSalida1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBoxControlPuerto As GroupBox
+    Friend WithEvents ButtonDesconectar As Button
+    Friend WithEvents ButtonConectar As Button
+    Friend WithEvents SerialPort As IO.Ports.SerialPort
+    Friend WithEvents GroupBoxSalida0 As GroupBox
+    Friend WithEvents ButtonSalida0On As Button
+    Friend WithEvents ButtonSalida0Off As Button
+    Friend WithEvents GroupBoxSalida1 As GroupBox
+    Friend WithEvents ButtonSalida1Off As Button
+    Friend WithEvents ButtonSalida1On As Button
+End Class
---
+++ CIN/Proyectos/ExamenCIN/ExamenCIN/Form1.vb
@@ -0,0 +1,202 @@
+﻿Imports System.IO.Ports
+
+Public Class Form1
+    Private Sub cargarForm(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Definimos tipo de letra de los objetos del formulario
+        Fuente_Personal = New Font("Verdana", 15, FontStyle.Bold, GraphicsUnit.Pixel)
+        'Habilito y deshabilito los Buttono conectar y desconectar
+        ButtonConectar.Enabled = True
+        ButtonDesconectar.Enabled = False
+
+        'Personalizamos nuestro formulario
+        With Me
+            .Font = Fuente_Personal
+            .BackColor = Color.AliceBlue
+            .Text = "COMUNICACIÓN SERIE SIN PROTOCOLO NX1P2"
+        End With
+
+        'Insertamos texto y color en los objetos
+        With ButtonConectar
+            .Text = "CONECTAR"
+            .BackColor = Color.Green
+        End With
+
+        With ButtonDesconectar
+            .Text = "DESCONECTAR"
+            .BackColor = Color.Red
+        End With
+
+        With ButtonSalida0On
+            .Text = "ON"
+            .BackColor = Color.Green
+        End With
+
+        With ButtonSalida0Off
+            .Text = "OFF"
+            .BackColor = Color.Red
+        End With
+
+        With ButtonSalida1On
+            .Text = "ON"
+            .BackColor = Color.Green
+        End With
+
+        With ButtonSalida1Off
+            .Text = "OFF"
+            .BackColor = Color.Red
+        End With
+
+        'Deshabilito los botones del groupbox_salida0
+        GroupBoxSalida0.Enabled = False
+        GroupBoxSalida1.Enabled = False
+    End Sub
+
+    Private Sub cerrandoForm(sender As Object, e As EventArgs) Handles Me.Closing
+        If SerialPort.IsOpen Then
+            Call DesconectarPuerto(sender, e)
+        End If
+    End Sub
+
+    Private Sub ConectarPuerto(sender As Object, e As EventArgs) Handles ButtonConectar.Click
+        'Utilizamos un bloque Try catch para capturar cualquier excepción que nos de windows
+        Try
+            'Configuro el puerto, le asigno el nombre y lo abrimos
+            With SerialPort
+                .BaudRate = 19200
+                .DataBits = 8
+                .Parity = IO.Ports.Parity.Odd
+                .StopBits = IO.Ports.StopBits.One
+                'Asigno el nombre del puerto que ha encontrado el procedimiento buscapuertos()
+                .PortName = "COM5"
+                'Abrimos el puerto Enviar
+                .Open()
+                'Si hemos podido abrirlo, lo indicamos
+                If .IsOpen Then
+                    .DiscardInBuffer()
+                    .DiscardOutBuffer()
+                    ButtonConectar.BackColor = Color.LimeGreen
+                    'Habilitamos el button desconectar y deshabilitamos el button conectar
+                    ButtonDesconectar.Enabled = True
+                    ButtonConectar.Enabled = False
+                    GroupBoxSalida0.Enabled = True
+                    GroupBoxSalida1.Enabled = True
+                Else
+                    'Si no hemos podido abrirlo lo indicamos con MsgBox
+                    MsgBox("FALLO EN LA CONEXIÓN", MsgBoxStyle.Critical)
+                    Beep()
+                End If
+            End With
+        Catch ex As Exception
+            'Capturo la excepción
+            MsgBox(ex.Message, MsgBoxStyle.Critical)
+        End Try
+    End Sub
+
+    Private Sub DesconectarPuerto(sender As Object, e As EventArgs) Handles ButtonDesconectar.Click
+        'Si el puerto está abierto
+        With SerialPort
+            If .IsOpen Then
+                'Limpia el puerto de salida y de entrada (Carácteres residuales)
+                .DiscardInBuffer()
+                .DiscardOutBuffer()
+                'Cerramos el puerto de Enviar
+                .Close()
+                'Llamamos de nuevo al form1_Load, para que todo quede igual
+                Call cargarForm(sender, e)
+            Else Me.OnClosing(e)
+                'Avisamos de que no estamos conectados
+                MsgBox("EL PUERTO NO ESTÁ ABIERTO", MsgBoxStyle.Exclamation)
+            End If
+        End With
+    End Sub
+
+    Private Sub enviarDatos(Trama_Saliente As Byte())
+        'Si el puerto está abierto
+        If SerialPort.IsOpen Then
+            'Voy a intentar enviar la trama de activar la salida 0 del PLC
+            Try
+                'Enviamos la trama de bytes por el puerto. Desde el componente 0 de la matriz
+                'hasta el último componente de la matriz.
+                SerialPort.Write(Trama_Saliente, 0, Trama_Saliente.Length) 'Enviar trama
+            Catch ex As Exception
+                MessageBox.Show(Me, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End If
+    End Sub
+
+    Private Sub encenderSalida0(sender As Object, e As EventArgs) Handles ButtonSalida0On.Click
+        Dim Trama_Saliente As Byte() 'Declaro la matriz de bytes
+        'La trama es: 34 F2 '@' >>> 34 y F2 son los dos bytes que vamos a transmitir
+        'El &H40 representa el carácter '@'
+        Trama_Saliente = New Byte(8) {
+                &HAA,
+                &HBB,
+                &HCC,
+                &HDD,
+                &H0,
+                &H0,
+                &H0,
+                &H0,
+                &H42} 'Asigno a la matriz 3 posiciones
+        'Enviamos la trama de bytes por el puerto. Desde el componente 0 de la matriz
+        'hasta el último componente de la matriz.
+        enviarDatos(Trama_Saliente)
+    End Sub
+
+    Private Sub apagarSalida0(sender As Object, e As EventArgs) Handles ButtonSalida0Off.Click
+        Dim Trama_Saliente As Byte() 'Declaro la matriz de bytes
+        'La trama es: 34 F2 '@' >>> 34 y F2 son los dos bytes que vamos a transmitir
+        'El &H40 representa el carácter '@'
+        Trama_Saliente = New Byte(8) {
+                &H0,
+                &H0,
+                &H0,
+                &H0,
+                &H11,
+                &H22,
+                &H33,
+                &H44,
+                &H42} 'Asigno a la matriz 3 posiciones
+        'Enviamos la trama de bytes por el puerto. Desde el componente 0 de la matriz
+        'hasta el último componente de la matriz.
+        enviarDatos(Trama_Saliente)
+    End Sub
+
+    Private Sub encenderSalida1(sender As Object, e As EventArgs) Handles ButtonSalida1On.Click
+        Dim Trama_Saliente As Byte() 'Declaro la matriz de bytes
+        'La trama es: 34 F2 '@' >>> 34 y F2 son los dos bytes que vamos a transmitir
+        'El &H40 representa el carácter '@'
+        Trama_Saliente = New Byte(8) {
+                &H1A,
+                &H1B,
+                &H1C,
+                &H1D,
+                &H0,
+                &H0,
+                &H0,
+                &H0,
+                &H42} 'Asigno a la matriz 3 posiciones
+        'Enviamos la trama de bytes por el puerto. Desde el componente 0 de la matriz
+        'hasta el último componente de la matriz.
+        enviarDatos(Trama_Saliente)
+    End Sub
+
+    Private Sub apagarSalida1(sender As Object, e As EventArgs) Handles ButtonSalida1Off.Click
+        Dim Trama_Saliente As Byte() 'Declaro la matriz de bytes
+        'La trama es: 34 F2 '@' >>> 34 y F2 son los dos bytes que vamos a transmitir
+        'El &H40 representa el carácter '@'
+        Trama_Saliente = New Byte(8) {
+                &H0,
+                &H0,
+                &H0,
+                &H0,
+                &HA1,
+                &HB1,
+                &HC1,
+                &HD1,
+                &H42} 'Asigno a la matriz 3 posiciones
+        'Enviamos la trama de bytes por el puerto. Desde el componente 0 de la matriz
+        'hasta el último componente de la matriz.
+        enviarDatos(Trama_Saliente)
+    End Sub
+End Class
